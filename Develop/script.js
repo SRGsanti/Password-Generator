26006360c34d834c81ff9466f4ce12b33b1191aa
SRGsanti <santiagor274@gmail.com>
2023-07-12 20:07:59 -0700

    new info

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,14 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+   function generatePassword(){
+    console.log("Look! You've clicked the Button!!");
+
+
+
+    return" User's Generated Password Will Go Here! ";
+   }
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
